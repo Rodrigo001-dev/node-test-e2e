a330a851212503ee87f35be511e8069a5150b679
Rodrigo <rodrigorael53@gmail.com>
2022-06-26 17:47:02 -0300

    running the prisma to create a migration

---
+++ prisma/migrations/20220626204132_create_lessons/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Lesson` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
